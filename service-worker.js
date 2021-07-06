2bfde4478445c95a92f12a783d8beec0b6f2ae87
Arrowana <8245419+Arrowana@users.noreply.github.com>
2021-07-06 19:28:17 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f4f07b2786f411c6e4864efe001bdc8.js"
+  "./precache-manifest.8583b36741f667383d6a76c54bb60935.js"
 );
 
 self.addEventListener('message', (event) => {
